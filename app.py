08e2a4f7599778cc0d8d1f319e6daf53a8a17e44
Alimurod777 <aliturdaliyev777@gmail.com>
2025-01-04 00:22:20 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Koyeb'
-
+    try:
+        return 'Hello from Koyeb'
+    except Exception as e:
+        return jsonify(error=str(e)), 500
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=80)
